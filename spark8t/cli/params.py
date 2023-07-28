ee051c2ecbb0e9205fdd7493457f10993d27b5c8
Enrico Deusebio <enrico.deusebio@canonical.com>
2023-07-28 16:03:35 +0200

    small fix

--- spark8t/cli/params.py
+++ spark8t/cli/params.py
@@ -132,7 +132,8 @@ def get_kube_interface(args: Namespace) -> AbstractKubeInterface:
         LightKube(args.kubeconfig or defaults.kube_config, defaults)
         if args.backend == "lightkube"
         else KubeInterface(
-            args.kubeconfig or defaults.kube_config, context_name=args.context
+            args.kubeconfig or defaults.kube_config, context_name=args.context,
+            kubectl_cmd=defaults.kubectl_cmd
         )
     )
 
